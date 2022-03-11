7ec97ac1123f98b6ba606b403870514ab00bac84
Jaiden Paterson <jaidenpaterson3@gmail.com>
2022-03-11 17:57:53 +1030

    coded a way to include all characters that are selected and ranomly selects them

--- Assets/generator.js
+++ Assets/generator.js
@@ -1,10 +1,46 @@
+
 var btn = document.querySelector("#btn")
+
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ADCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numChar = "123456789";
+var specialChar = "!@#$%^&*()_+-=;:<>?/";
 
 //create function on button click
 function startGenerating(length, lower, upper, num, special){
 
-    console.log(length,lower,upper,num,special);
-}
+    //create a division counter variable
+    var count = 0;
+    var divLength = length / 4;
+    var lowerArr = [];
+
+    if (lower == 'yes'){
+
+            count++;
+            var arrFromLower = Array.from(lowerChar);
+
+            for (i = 0; i < divLength; i++){
+                lowerArr.push(arrFromLower[Math.floor(Math.random() * arrFromLower.length)]);
+
+                console.log(lowerArr.join(""));
+            }
+
+            
+
+        }
+        
+    }
+    
+    //each character type is it's own variable
+    
+    //find out what character type is included
+    
+    //if all character types are included length x 4
+    //use the divided number and randomly find the amount of characters
+    //then concact all variables together
+
+    
+
 
 
 // on button click display prompts
@@ -30,13 +66,17 @@ btn.addEventListener('click', function(){
              
            if(num == "no" || lower === "yes"){
 
-               special = prompt("Would you like to inlcude special characters?").toLowerCase()
+           special = prompt("Would you like to inlcude special characters?").toLowerCase()
 
-               startGenerating(length, lower, upper, num, special);
+           startGenerating(length, lower, upper, num, special);
+
            }
-           }
+        }
     }
 }
 
 })
 
+//TODO :: Need to set length to array = to the users wants, find a way to include all slecected characters and ranomly join them into the string
+
+//3/10/22 PLAN math.random the divisble length of string to the array'd variables then concatinate strings, iterate math.random until it has number of values correact
